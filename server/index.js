c1479f73a7a84bb5e3eb24caa88d51339bf50d8d
Eddie <arrizonedgar91@gmail.com>
2022-02-01 15:31:25 -1000

    setup middleware on serverfile

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 5000;
+
+app.use(express.json());
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server listening on ${port}`)
