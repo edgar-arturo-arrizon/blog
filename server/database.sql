1398ec8866d53cff6df3b26f0d6366c323dc5a92
Eddie <arrizonedgar91@gmail.com>
2022-02-01 16:02:18 -1000

    database file set up

---
+++ server/database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE BlogSystem;
+
+CREATE TABLE users(
+user_id uuid PRIMARY KEY DEFAULT
+uuid_generate_v4(),
+user_name VARCHAR(35) NOT NULL,
+user_email VARCHAR(50) NOT NULL,
+user_password VARCHAR(25) NOT NULL
+);
+
+INSERT INTO users (user_name, user_email, user_password) VALUES ('edgar', 'edgarrizon@gmail.com', 'ea1991');
