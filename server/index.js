efef358ed863baa0edad0e29c6243ba060d023f5
Eddie <arrizonedgar91@gmail.com>
2022-02-01 16:40:26 -1000

    added routes folder and jwtAuth file

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import cors from 'cors';
+import router from './routes/jwtAuth.js';
 
 const app = express();
 const port = 5000;
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/auth', () => {
+  console.log('test');
+});
 
 app.listen(port, () => {
   console.log(`Server listening on ${port}`)
---
+++ server/routes/jwtAuth.js
@@ -0,0 +1,25 @@
+import express from 'express';
+
+const router = express.Router();
+
+// router.post('/', async (req, res) => {
+//   try {
+
+// //step 1 destructure req.body eamil/password/name
+
+// //step 2 check if user exists, if they do throw error
+
+// //step 3 else  bcrypt user password
+
+// //step 4 enter user in db
+
+// //step 5 generating our jwt token
+
+
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send("server Error ")
+//   }
+// })
+
+export default router;
