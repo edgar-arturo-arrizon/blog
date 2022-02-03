38519ed4b283b584f9cb889ed1795055441dbde0
Eddie <arrizonedgar91@gmail.com>
2022-02-03 10:26:01 -1000

    jwt generator done

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,27 +1,28 @@
 import express from 'express';
 import pool from '../db.js';
 import bcrypt from 'bcrypt';
+import jwtGenerator from '../utils/jwtGenerator.js';
 
 const router = express.Router();
 
 //registering
 router.post('/register', async (req, res) => {
   try {
-  //step 1 destructure req.body eamil/password/name
+    //step 1 destructure req.body eamil/password/name
     const { name, email, password } = req.body;
     const user = await pool.query('SELECT * FROM users WHERE user_email = $1', [email]);
-    //step 2 if user exist throw error
+
     if (user.rows.length !== 0) {
       return res.status(401).send('User already exists');
     }
-    //step 3 else bcrypt user password
+
     const salt = await bcrypt.genSalt(10);
     const bcryptPassword = await bcrypt.hash(password, salt);
-    console.log(bcryptPassword)
-  //step 4 enter user in db
+
     const newUser = await pool.query("INSERT INTO users (user_name, user_email, user_password) Values ($1, $2, $3) RETURNING *", [ name, email, bcryptPassword ]);
 
-    res.json(newUser.rows[0])
+    const token = jwtGenerator(newUser.rows[0].user_id)
+    res.json({ token });
 
   //step 5 generating our jwt token
 
--- server/utils/jwtGenerator.js
+++ server/utils/jwtGenerator.js
@@ -1,13 +1,14 @@
 import jwt from 'jsonwebtoken';
 import dotenv from "dotenv";
+
 dotenv.config()
 
-function jwtGenerator() {
+function jwtGenerator(user_id) {
   const payload = {
-    user: user_id,
+    id: user_id,
   }
 
-  return jwt.sign(payload, process.env.jwtSecret, {expiresIn: "1hr"})
+  return jwt.sign(payload, process.env.jwtSecret, { expiresIn: "1hr" })
 }
 
 export default jwtGenerator;
