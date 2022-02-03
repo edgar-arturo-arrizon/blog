2b1d7fac96cb62fed8af6c2ac52bac28b876da4f
Eddie <arrizonedgar91@gmail.com>
2022-02-03 10:39:23 -1000

    login route finished

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -8,7 +8,6 @@ const router = express.Router();
 //registering
 router.post('/register', async (req, res) => {
   try {
-    //step 1 destructure req.body eamil/password/name
     const { name, email, password } = req.body;
     const user = await pool.query('SELECT * FROM users WHERE user_email = $1', [email]);
 
@@ -28,8 +27,36 @@ router.post('/register', async (req, res) => {
 
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("server Error ")
+    res.status(500).send('server Error')
   }
-})
+});
+
+//login route
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [ email ]);
+
+    if(user.rows.length === 0) {
+      return res.status(401).send('Password or email is incorrect');
+    }
+
+    const validPassword = await bcrypt.compare(password, user.rows[0].user_password);
+
+    if(!validPassword) {
+      return res.status(401).json('Password or Email is incorrect');
+    }
+
+    const token = jwtGenerator(user.rows[0].user_id);
+
+    res.json({ token });
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 export default router;
