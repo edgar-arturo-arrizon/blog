ed454815801e30632c6cb217fe319d0877f771e6
Eddie <arrizonedgar91@gmail.com>
2022-02-02 16:31:55 -1000

    able to add new user into db

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import pool from '../db.js';
+import bcrypt from 'bcrypt';
 
 const router = express.Router();
 
@@ -7,14 +8,13 @@ const router = express.Router();
 
 router.post('/register', async (req, res) => {
   try {
-    console.log('testing', req.body)
   //step 1 destructure req.body eamil/password/name
     const { name, email, password } = req.body;
     const user = await pool.query('SELECT * FROM users WHERE user_email = $1', [email]);
 
     //step 2 if user exist throw error
     if (user.rows.length !== 0) {
-      res.status(401).send('User already exists');
+      return res.status(401).send('User already exists');
     }
 
     //step 3 else bcrypt user password
@@ -24,6 +24,10 @@ router.post('/register', async (req, res) => {
     const bcryptPassword = bcrypt.hash(password, salt);
 
   //step 4 enter user in db
+
+    const newUser = await pool.query("INSERT INTO users (user_name, user_email, user_password) Values ($1, $2, $3) RETURNING *", [ name, email, bcryptPassword ]);
+
+    res.json(newUser)
 
   //step 5 generating our jwt token
 
