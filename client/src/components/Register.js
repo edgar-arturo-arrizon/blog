8bb8b42ae03dd893ff9ae64f285bedf14d4ebcad
Eddie <arrizonedgar91@gmail.com>
2022-02-05 17:26:59 -1000

    register form submission not working

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,33 +1,65 @@
-import React, { Fragment } from 'react';
+import React, { useState } from 'react';
 
-const Register = () => {
+const Register = ( { setAuth }) => {
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+    name: ''
+  });
+
+  const { email, password, name } = inputs;
+
+  const handleChange = (e) => {
+    setInputs({...inputs, [e.target.name] : e.target.value})
+  }
+
+  const onSubmitForm = async (e) => {
+    console.log(inputs);
+    e.preventDefault();
+    try {
+      const body = { email, password, name };
+
+      const response = await fetch('http://localhost:5000/auth/register', {
+        method: 'POST',
+        header: { 'Content-Type': 'application/json;charset=utf-8' },
+        body: JSON.stringify(body)
+      });
+
+      const parseRes = await response.json();
+
+      console.log('test',parseRes)
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
   return (
-    <Fragment>
+    <>
       <h1>Register</h1>
-      <form className=" border-cyan-500 border-2">
-        <input
-          type='text'
-          name='name'
-          value={'name'}
-          placeholder='name'
-          onChange={(e) => console.log(e)}
-        />
+      <form onSubmit={onSubmitForm} className=" border-cyan-500 border-2">
         <input
           type='text'
           name='email'
-          value={'email'}
+          value={email}
           placeholder='email'
-          onChange={(e) => console.log(e)}
+          onChange={e => handleChange(e)}
         />
         <input
           type='text'
           name='password'
-          value={'password'}
+          value={password}
           placeholder='password'
-          onChange={(e) => console.log(e)}
+          onChange={e => handleChange(e)}
         />
+        <input
+          type='text'
+          name='name'
+          value={name}
+          placeholder='name'
+          onChange={e => handleChange(e)}
+        />
+        <button>Submit</button>
       </form>
-    </ Fragment>
+    </>
   )
 }
 
